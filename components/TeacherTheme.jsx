2487291cc942c5742140584ced789012b493df53
bee4739 <beenoii.rn@gmail.com>
2021-03-23 22:06:17 +0700

    create form addsubject

--- components/TeacherTheme.jsx
+++ components/TeacherTheme.jsx
@@ -28,43 +28,43 @@ import AssignmentIcon from "@material-ui/icons/Assignment";
 import Link from "next/link";
 const drawerWidth = 240;
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex",
+    display: "flex"
   },
   toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
+    paddingRight: 24 // keep right padding when drawer closed
   },
   toolbarIcon: {
     display: "flex",
     alignItems: "center",
     justifyContent: "flex-end",
     padding: "0 8px",
-    ...theme.mixins.toolbar,
+    ...theme.mixins.toolbar
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: 36
   },
   menuButtonHidden: {
-    display: "none",
+    display: "none"
   },
   title: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   drawerPaper: {
     position: "relative",
@@ -72,39 +72,39 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   drawerPaperClose: {
     overflowX: "hidden",
     transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
     width: theme.spacing(7),
     [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9),
-    },
+      width: theme.spacing(9)
+    }
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     height: "100vh",
-    overflow: "auto",
+    overflow: "auto"
   },
   container: {
     paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
   },
   paper: {
     padding: theme.spacing(2),
     display: "flex",
     overflow: "auto",
-    flexDirection: "column",
+    flexDirection: "column"
   },
   fixedHeight: {
-    height: 240,
-  },
+    height: 240
+  }
 }));
 
 export default function TeacherTheme(props) {
@@ -160,7 +160,7 @@ export default function TeacherTheme(props) {
           paper: clsx(
             classes.drawerPaper,
             !props.openNav && classes.drawerPaperClose
-          ),
+          )
         }}
         open={props.openNav}
       >
@@ -177,7 +177,7 @@ export default function TeacherTheme(props) {
                 <ListItemIcon>
                   <DashboardIcon />
                 </ListItemIcon>
-                <ListItemText primary="กลุ่มเรียน" />
+                <ListItemText primary="รายวิชา" />
               </ListItem>
             </Link>
 
--- pages/teacher/subject/index.jsx
+++ pages/teacher/subject/index.jsx
@@ -1,16 +1,116 @@
 import React from "react";
 import { useRouter } from "next/router";
 import TeacherTheme from "../../../components/TeacherTheme";
+import { makeStyles } from "@material-ui/core/styles";
+import { useForm, Controller } from "react-hook-form";
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    textAlign: "right"
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  }
+}));
 
 export default function Teacher(props) {
+  const classes = useStyles();
   const router = useRouter();
+  const { control, handleSubmit } = useForm();
+
+  const onSubmit = data => {
+    axios
+      .post(`${props.env.api_url}/login`, JSON.stringify(data))
+      .then(value => {
+        console.log(value.data);
+        if (value.data.success) {
+          props.setUserLogin(value.data.data);
+          router.replace("/");
+        } else {
+          alert(value.data.message);
+        }
+      })
+      .catch(reason => {
+        console.log(reason);
+      });
+  };
 
   React.useEffect(() => {}, []);
 
   return (
     <TeacherTheme {...props}>
-      <form>
-        <button>Add</button>
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className={classes.button}>
+          <button
+            type="button"
+            className="btn btn-success"
+            data-toggle="modal"
+            data-target="#AddSub"
+          >
+            เพิ่มรายวิชา
+          </button>
+        </div>
+
+        <div
+          className="modal fade"
+          id="AddSub"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  เพิ่มข้อมูลรายวิชา
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="row">
+                  <div className="col-sm-4 mb-2 mt-2 align-middle text-right">
+                    <label>รหัสวิชา : </label>
+                  </div>
+                  <div className="col-sm-8 mb-2 mt-2 align-middle text-left">
+                    <input type="text" className="form-control"></input>
+                  </div>
+                  <div className="col-sm-4 mb-2 mt-2 align-middle text-right">
+                    <label>ชื่อวิชาภาษาไทย : </label>
+                  </div>
+                  <div className="col-sm-8 mb-2 mt-2 align-middle text-left">
+                    <input type="text" className="form-control"></input>
+                  </div>
+                  <div className="col-sm-4 mb-2 mt-2 align-middle text-right">
+                    <label>ชื่อวิชาภาษาอังกฤษ : </label>
+                  </div>
+                  <div className="col-sm-8 mb-2 mt-2 align-middle text-left">
+                    <input type="text" className="form-control"></input>
+                  </div>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-dismiss="modal"
+                >
+                  ยกเลิก
+                </button>
+                <button type="button" className="btn btn-success">
+                  เพิ่ม
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
     </TeacherTheme>
   );
