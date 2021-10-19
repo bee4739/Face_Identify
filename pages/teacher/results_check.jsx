8c11895f8a0651e107da01b401e25b6007f39539
bee4739 <beenoii.rn@gmail.com>
2021-10-19 16:20:12 +0700

    backup

--- pages/teacher/results_check.jsx
+++ pages/teacher/results_check.jsx
@@ -3,75 +3,152 @@ import { useRouter } from "next/router";
 import TeacherTheme from "../../components/TeacherTheme";
 import { makeStyles } from "@material-ui/core/styles";
 import { useForm, Controller } from "react-hook-form";
+import { useEffect, useState } from "react";
+import axios from "axios";
+// import '../node_modules/boostrap/dist/css/boostrap.min.css';
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   button: {
-    textAlign: "center",
+    textAlign: "center"
   },
   form: {
     width: "80%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
+    marginTop: theme.spacing(1)
+  }
 }));
 
 export default function Teacher(props) {
   const classes = useStyles();
   const router = useRouter();
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit, handleChange } = useForm();
+  const [ddyear, setddYear] = useState("");
+  const [ddsubject, setddSubject] = useState("");
 
-  const onSubmit = (data) => {
+  const [year, setYear] = useState([]);
+  const getYear = data => {
     axios
-      .post(`${props.env.api_url}/login`, JSON.stringify(data))
-      .then((value) => {
-        console.log(value.data);
-        if (value.data.success) {
-          props.setUserLogin(value.data.data);
-          router.replace("/");
-        } else {
-          alert(value.data.message);
-        }
+      .post(`${props.env.api_url}/resultcheck`, JSON.stringify(data))
+      .then(value => {
+        setYear(value.data.result);
+        console.log("ssss", value.data.result);
       })
-      .catch((reason) => {
+      .catch(reason => {
         console.log(reason);
       });
   };
 
-  React.useEffect(() => {}, []);
+  const [subject, setSubject] = useState([]);
+  const getSubject = data => {
+    axios
+      .post(`${props.env.api_url}/resultchecksubject`, JSON.stringify(data))
+      .then(value => {
+        setSubject(value.data.result);
+        console.log("ssss", value.data.result);
+      })
+      .catch(reason => {
+        console.log(reason);
+      });
+  };
+
+  const [showresulte, setShowresulte] = useState([]);
+  const getShowresulte = data => {
+    // data = { ...data, Year_ID: ddyear.Year_ID, Subject_PK: ddsubject.Subject_PK };
+    axios
+      .post(`${props.env.api_url}/showresultcheck`, JSON.stringify(data))
+      .then(value => {
+        setShowresulte(value.data.result);
+        console.log("ssss", value.data.result);
+      })
+      .catch(reason => {
+        console.log(reason);
+      });
+  };
+
+  React.useEffect(() => {
+    getYear();
+    getSubject();
+  }, []);
 
   return (
     <TeacherTheme {...props}>
-      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className="row">
-          <div className="col-sm-4 mb-2 mt-2 align-middle text-right">
-            ปีการศึกษา :
-          </div>
-          <div className="col-sm-6 mb-2 mt-2 align-middle text-left">
-            <div className={classes.d}>
-              <select class="form-control form-control-sm">
-                <option>เลือกปีการศึกษา</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-              </select>
+      <form
+      //className={classes.form}
+      //onSubmit={handleSubmit(getShowresulte)}
+      >
+        <div className={classes.form}>
+          <div className="row">
+            <div className="col-sm-5 mt-2 align-middle text-right">
+              <label>ปีการศึกษา : </label>
             </div>
-          </div>
-          <div className="col-sm-4 mb-2 mt-2 align-middle text-right">
-            ภาคเรียน :
-          </div>
-          <div className="col-sm-6 mb-2 mt-2 align-middle text-left">
-            <div className={classes.d}>
-              <select class="form-control form-control-sm">
-                <option>เลือกภาคเรียน</option>
-                <option>ภาคเรียนที่ 1</option>
-                <option>ภาคเรียนที่ 2</option>
-                <option>ภาคเรียนฤดูร้อน</option>
-              </select>
+            <div className="col-sm-6 mt-2 mb-2 align-middle text-left">
+              <Controller
+                name="Year_ID"
+                defaultValue=""
+                control={control}
+                variant="outlined"
+                // onChange={setddYear(value)}
+                render={({ onChange, value }) => (
+                  <select
+                    className="form-control"
+                    onChange={onChange}
+                    //onSelect={() => {
+                    //  setddYear(value);
+                    //}}
+                    value={value}
+                  >
+                    <option value="" disabled="disabled">
+                      กรุณาเลือกปีการศึกษา...
+                    </option>
+                    {year.map((variable, index) => {
+                      return (
+                        <option key={index} value={variable.Year_ID}>
+                          {variable.Year} - {variable.Term}
+                        </option>
+                      );
+                    })}
+                  </select>
+                )}
+              />
+            </div>
+            <div className="col-sm-5 mt-2 align-middle text-right">
+              <label>วิชา : </label>
+            </div>
+            <div className="col-sm-6 mt-2 mb-2 align-middle text-left">
+              <Controller
+                name="Subject_ID"
+                defaultValue=""
+                control={control}
+                variant="outlined"
+                //onChange={setddSubject(value)}
+                render={({ onChange, value }) => (
+                  <select
+                    className="form-control"
+                    onChange={onChange}
+                    //onSelect={() => {
+                    //  setddSubject(value);
+                    //}}
+                    value={value}
+                  >
+                    <option value="" disabled="disabled">
+                      กรุณาเลือกวิชา...
+                    </option>
+                    {subject.map((variable, index) => {
+                      return (
+                        <option key={index} value={variable.Subject_PK}>
+                          {variable.Subject_ID} - {variable.Subject_NameTH}
+                        </option>
+                      );
+                    })}
+                  </select>
+                )}
+              />
             </div>
           </div>
         </div>
         <div className={classes.button}>
           <button
-            type="button"
+            type="submit"
             className="btn btn-success ml-2 mr-2 mb-4 mt-4 "
             data-toggle="modal"
             data-target="#AddSub"
@@ -80,6 +157,53 @@ export default function Teacher(props) {
           </button>
         </div>
       </form>
+
+      <div className="col-sm-12  mt-4 align-middle">
+        <table className="table table-striped align-middle text-center" id="ex">
+          <thead>
+            <tr style={{ height: "60px" }}>
+              <th
+                style={{ verticalAlign: "middle", backgroundColor: "#DDDDDD" }}
+              >
+                ชื่อ-นามสกุล
+              </th>
+              <th
+                style={{ verticalAlign: "middle", backgroundColor: "#DDDDDD" }}
+              >
+                วันที่
+              </th>
+              <th
+                style={{ verticalAlign: "middle", backgroundColor: "#DDDDDD" }}
+              >
+                สถานะ
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {showresulte.map((variable, index) => {
+              return (
+                <tr key={index}>
+                  <td>
+                    {variable.Std_Title}
+                    {variable.Std_FirstName} {variable.Std_LastName}
+                  </td>
+                  <td>{variable.Date}</td>
+                  <td>{variable.Status}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className={classes.button}>
+        <ReactHTMLTableToExcel
+          className="btn btn-info mt-4"
+          table="ex"
+          filename="Emp"
+          sheet="Sheet"
+          buttonText="Export"
+        />
+      </div>
     </TeacherTheme>
   );
 }
